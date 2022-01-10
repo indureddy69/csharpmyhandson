d2c915df093578d204e922347ded7dc02fca55ac
indureddy69 <indureddy4044@gmail.com>
2022-01-10 10:57:20 +0530

    datetime code

---
+++ ConsoleApp6/Program7.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp6
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+            Console.WriteLine("id      " + "source        " + "destination              " + "date             " + "status           " + "network          ");
+            FileStream fs = new FileStream("C:/Users/91938/Desktop/cg.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            while (sr.Peek() > 0)
+            {
+                string readmyline = sr.ReadLine();
+                string[] strings = readmyline.Split(':');
+                if (readmyline != "")
+                {
+
+                    if (strings.Length > 1)
+                    {
+                        if (readmyline.StartsWith("Date"))
+                        {
+
+                            string date = strings[1].Split(' ')[0];
+                            Console.WriteLine(date + "       ");
+                        }
+                        else
+                        {
+                            Console.Write(strings[1] + "        ");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+
+
+
+                }
+            }
+        }
+    }
+}
+    
+
+        
+
