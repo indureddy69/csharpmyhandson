e8fbcd855da9a4ef3ca8af8cacf6cdb9354c279f
indureddy69 <indureddy4044@gmail.com>
2022-01-10 10:52:22 +0530

    range number

---
+++ rangenumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rangenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int> { 30, 40, 50, 60 ,70, 80, 90 ,120,150,32,21,11};
+            var SelectedNumbers = Numbers.Where(X => X > 30).Where(X => X < 100);
+            foreach(var s in SelectedNumbers)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
