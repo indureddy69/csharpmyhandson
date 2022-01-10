bc1bc89555c5029e6fbb1f16095b1255d7feb689
indureddy69 <indureddy4044@gmail.com>
2022-01-10 10:00:41 +0530

    my handson code

---
+++ reverse string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Demo
+{
+    static void Main()
+    {
+        string myStr, rev;
+        myStr = "Indu";
+        rev = "";
+        Console.WriteLine("String is {0}", myStr);
+        // find string length
+        int len;
+        len = myStr.Length - 1;
+        while (len >= 0)
+        {
+            rev = rev + myStr[len];
+            len--;
+        }
+        Console.WriteLine("Reversed String is {0}", rev);
+        Console.ReadLine();
+    }
+}
